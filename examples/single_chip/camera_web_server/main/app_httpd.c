4c05b5bf8555ae19fedfd0b7019707c924af7013
Schleichdie <Lucas.Lenarcic@gmail.com>
2022-08-01 13:29:55 +0200

    Esp-Who Copy: Testing

--- examples/single_chip/camera_web_server/main/app_httpd.c
+++ examples/single_chip/camera_web_server/main/app_httpd.c
@@ -1427,30 +1427,273 @@ static esp_err_t monitor_handler(httpd_req_t *req)
 
 static esp_err_t name_handler(httpd_req_t *req)
 {
+    
     char *buf = NULL;
+    char variable[32];
+    char value[32];
 
     if (parse_get(req, &buf) != ESP_OK) {
         return ESP_FAIL;
     }
-
-    int startX = parse_get_var(buf, "sx", 0);
-    int startY = parse_get_var(buf, "sy", 0);
-    int endX = parse_get_var(buf, "ex", 0);
-    int endY = parse_get_var(buf, "ey", 0);
-    int offsetX = parse_get_var(buf, "offx", 0);
-    int offsetY = parse_get_var(buf, "offy", 0);
-    int totalX = parse_get_var(buf, "tx", 0);
-    int totalY = parse_get_var(buf, "ty", 0);
-    int outputX = parse_get_var(buf, "ox", 0);
-    int outputY = parse_get_var(buf, "oy", 0);
-    bool scale = parse_get_var(buf, "scale", 0) == 1;
-    bool binning = parse_get_var(buf, "binning", 0) == 1;
+    if (httpd_query_key_value(buf, "var", variable, sizeof(variable)) != ESP_OK ||
+        httpd_query_key_value(buf, "val", value, sizeof(value)) != ESP_OK) {
+        free(buf);
+        httpd_resp_send_404(req);
+        return ESP_FAIL;
+    }
     free(buf);
 
-    ESP_LOGI(TAG, "Set Window: Start: %d %d, End: %d %d, Offset: %d %d, Total: %d %d, Output: %d %d, Scale: %u, Binning: %u", startX, startY, endX, endY, offsetX, offsetY, totalX, totalY, outputX, outputY, scale, binning);
+    int val = atoi(value);
+    ESP_LOGI(TAG, "%s = %d", variable, val);
     sensor_t *s = esp_camera_sensor_get();
-    int res = s->set_res_raw(s, startX, startY, endX, endY, offsetX, offsetY, totalX, totalY, outputX, outputY, scale, binning);
-    int res2 = s->set_res_raw(s, 1, startY, endX, endY, offsetX, offsetY, totalX, totalY, outputX, outputY, scale, binning);
+    int res = 0;
+
+    if (!strcmp(variable, "framesize")) {
+        if (s->pixformat == PIXFORMAT_JPEG) {
+            res = s->set_framesize(s, (framesize_t)val);
+            if (res == 0) {
+                app_mdns_update_framesize(val);
+            }
+        }
+    }
+    else if (!strcmp(variable, "quality"))
+        res = s->set_quality(s, val);
+    else if (!strcmp(variable, "contrast"))
+        res = s->set_contrast(s, val);
+    else if (!strcmp(variable, "brightness"))
+        res = s->set_brightness(s, val);
+    else if (!strcmp(variable, "saturation"))
+        res = s->set_saturation(s, val);
+    else if (!strcmp(variable, "gainceiling"))
+        res = s->set_gainceiling(s, (gainceiling_t)val);
+    else if (!strcmp(variable, "colorbar"))
+        res = s->set_colorbar(s, val);
+    else if (!strcmp(variable, "awb"))
+        res = s->set_whitebal(s, val);
+    else if (!strcmp(variable, "agc"))
+        res = s->set_gain_ctrl(s, val);
+    else if (!strcmp(variable, "aec"))
+        res = s->set_exposure_ctrl(s, val);
+    else if (!strcmp(variable, "hmirror"))
+        res = s->set_hmirror(s, val);
+    else if (!strcmp(variable, "vflip"))
+        res = s->set_vflip(s, val);
+    else if (!strcmp(variable, "awb_gain"))
+        res = s->set_awb_gain(s, val);
+    else if (!strcmp(variable, "agc_gain"))
+        res = s->set_agc_gain(s, val);
+    else if (!strcmp(variable, "aec_value"))
+        res = s->set_aec_value(s, val);
+    else if (!strcmp(variable, "aec2"))
+        res = s->set_aec2(s, val);
+    else if (!strcmp(variable, "dcw"))
+        res = s->set_dcw(s, val);
+    else if (!strcmp(variable, "bpc"))
+        res = s->set_bpc(s, val);
+    else if (!strcmp(variable, "wpc"))
+        res = s->set_wpc(s, val);
+    else if (!strcmp(variable, "raw_gma"))
+        res = s->set_raw_gma(s, val);
+    else if (!strcmp(variable, "lenc"))
+        res = s->set_lenc(s, val);
+    else if (!strcmp(variable, "special_effect"))
+        res = s->set_special_effect(s, val);
+    else if (!strcmp(variable, "wb_mode"))
+        res = s->set_wb_mode(s, val);
+    else if (!strcmp(variable, "ae_level"))
+        res = s->set_ae_level(s, val);
+#ifdef CONFIG_LED_ILLUMINATOR_ENABLED
+    else if (!strcmp(variable, "led_intensity")) {
+        led_duty = val;
+        if (isStreaming)
+            enable_led(true);
+    }
+#endif
+
+#if CONFIG_ESP_FACE_DETECT_ENABLED
+    else if (!strcmp(variable, "face_detect")) {
+        detection_enabled = val;
+#if CONFIG_ESP_FACE_RECOGNITION_ENABLED
+        if (!detection_enabled) {
+            recognition_enabled = 0;
+        }
+#endif
+    }
+#if CONFIG_ESP_FACE_RECOGNITION_ENABLED
+    else if (!strcmp(variable, "face_enroll"))
+        is_enrolling = val;
+        ESP_LOGI(TAG, val)
+    else if (!strcmp(variable, "face_recognize")) {
+        recognition_enabled = val;
+        if (recognition_enabled) {
+            detection_enabled = val;
+        }
+    }
+#endif
+#endif
+    else {
+        ESP_LOGI(TAG, "Unknown command: %s", variable);
+        res = -1;
+    }
+
+    if (res < 0) {
+        return httpd_resp_send_500(req);
+    }
+
+    httpd_resp_set_hdr(req, "Access-Control-Allow-Origin", "*");
+    return httpd_resp_send(req, NULL, 0);
+}
+
+static int print_reg(char * p, sensor_t * s, uint16_t reg, uint32_t mask){
+    return sprintf(p, "\"0x%x\":%u,", reg, s->get_reg(s, reg, mask));
+}
+
+static esp_err_t status_handler(httpd_req_t *req)
+{
+    static char json_response[1024];
+
+    sensor_t *s = esp_camera_sensor_get();
+    char *p = json_response;
+    *p++ = '{';
+
+    if(s->id.PID == OV5640_PID || s->id.PID == OV3660_PID){
+        for(int reg = 0x3400; reg < 0x3406; reg+=2){
+            p+=print_reg(p, s, reg, 0xFFF);//12 bit
+        }
+        p+=print_reg(p, s, 0x3406, 0xFF);
+
+        p+=print_reg(p, s, 0x3500, 0xFFFF0);//16 bit
+        p+=print_reg(p, s, 0x3503, 0xFF);
+        p+=print_reg(p, s, 0x350a, 0x3FF);//10 bit
+        p+=print_reg(p, s, 0x350c, 0xFFFF);//16 bit
+
+        for(int reg = 0x5480; reg <= 0x5490; reg++){
+            p+=print_reg(p, s, reg, 0xFF);
+        }
+
+        for(int reg = 0x5380; reg <= 0x538b; reg++){
+            p+=print_reg(p, s, reg, 0xFF);
+        }
+
+        for(int reg = 0x5580; reg < 0x558a; reg++){
+            p+=print_reg(p, s, reg, 0xFF);
+        }
+        p+=print_reg(p, s, 0x558a, 0x1FF);//9 bit
+    } else if(s->id.PID == OV2640_PID){
+        p+=print_reg(p, s, 0xd3, 0xFF);
+        p+=print_reg(p, s, 0x111, 0xFF);
+        p+=print_reg(p, s, 0x132, 0xFF);
+    }
+
+    p += sprintf(p, "\"board\":\"%s\",", CAM_BOARD);
+    p += sprintf(p, "\"xclk\":%u,", s->xclk_freq_hz / 1000000);
+    p += sprintf(p, "\"pixformat\":%u,", s->pixformat);
+    p += sprintf(p, "\"framesize\":%u,", s->status.framesize);
+    p += sprintf(p, "\"quality\":%u,", s->status.quality);
+    p += sprintf(p, "\"brightness\":%d,", s->status.brightness);
+    p += sprintf(p, "\"contrast\":%d,", s->status.contrast);
+    p += sprintf(p, "\"saturation\":%d,", s->status.saturation);
+    p += sprintf(p, "\"sharpness\":%d,", s->status.sharpness);
+    p += sprintf(p, "\"special_effect\":%u,", s->status.special_effect);
+    p += sprintf(p, "\"wb_mode\":%u,", s->status.wb_mode);
+    p += sprintf(p, "\"awb\":%u,", s->status.awb);
+    p += sprintf(p, "\"awb_gain\":%u,", s->status.awb_gain);
+    p += sprintf(p, "\"aec\":%u,", s->status.aec);
+    p += sprintf(p, "\"aec2\":%u,", s->status.aec2);
+    p += sprintf(p, "\"ae_level\":%d,", s->status.ae_level);
+    p += sprintf(p, "\"aec_value\":%u,", s->status.aec_value);
+    p += sprintf(p, "\"agc\":%u,", s->status.agc);
+    p += sprintf(p, "\"agc_gain\":%u,", s->status.agc_gain);
+    p += sprintf(p, "\"gainceiling\":%u,", s->status.gainceiling);
+    p += sprintf(p, "\"bpc\":%u,", s->status.bpc);
+    p += sprintf(p, "\"wpc\":%u,", s->status.wpc);
+    p += sprintf(p, "\"raw_gma\":%u,", s->status.raw_gma);
+    p += sprintf(p, "\"lenc\":%u,", s->status.lenc);
+    p += sprintf(p, "\"hmirror\":%u,", s->status.hmirror);
+    p += sprintf(p, "\"dcw\":%u,", s->status.dcw);
+    p += sprintf(p, "\"colorbar\":%u", s->status.colorbar);
+#ifdef CONFIG_LED_ILLUMINATOR_ENABLED
+    p += sprintf(p, ",\"led_intensity\":%u", led_duty);
+#else
+    p += sprintf(p, ",\"led_intensity\":%d", -1);
+#endif
+#if CONFIG_ESP_FACE_DETECT_ENABLED
+    p += sprintf(p, ",\"face_detect\":%u", detection_enabled);
+#if CONFIG_ESP_FACE_RECOGNITION_ENABLED
+    p += sprintf(p, ",\"face_enroll\":%u,", is_enrolling);
+    p += sprintf(p, "\"face_recognize\":%u", recognition_enabled);
+#endif
+#endif
+    *p++ = '}';
+    *p++ = 0;
+    httpd_resp_set_type(req, "application/json");
+    httpd_resp_set_hdr(req, "Access-Control-Allow-Origin", "*");
+    return httpd_resp_send(req, json_response, strlen(json_response));
+}
+
+static esp_err_t mdns_handler(httpd_req_t *req)
+{
+    size_t json_len = 0;
+    const char * json_response = app_mdns_query(&json_len);
+    httpd_resp_set_type(req, "application/json");
+    httpd_resp_set_hdr(req, "Access-Control-Allow-Origin", "*");
+    return httpd_resp_send(req, json_response, json_len);
+}
+
+static esp_err_t xclk_handler(httpd_req_t *req)
+{
+    char *buf = NULL;
+    char _xclk[32];
+
+    if (parse_get(req, &buf) != ESP_OK) {
+        return ESP_FAIL;
+    }
+    if (httpd_query_key_value(buf, "xclk", _xclk, sizeof(_xclk)) != ESP_OK) {
+        free(buf);
+        httpd_resp_send_404(req);
+        return ESP_FAIL;
+    }
+    free(buf);
+
+    int xclk = atoi(_xclk);
+    ESP_LOGI(TAG, "Set XCLK: %d MHz", xclk);
+
+    sensor_t *s = esp_camera_sensor_get();
+    int res = s->set_xclk(s, LEDC_TIMER_0, xclk);
+    if (res) {
+        return httpd_resp_send_500(req);
+    }
+
+    httpd_resp_set_hdr(req, "Access-Control-Allow-Origin", "*");
+    return httpd_resp_send(req, NULL, 0);
+}
+
+static esp_err_t reg_handler(httpd_req_t *req)
+{
+    char *buf = NULL;
+    char _reg[32];
+    char _mask[32];
+    char _val[32];
+
+    if (parse_get(req, &buf) != ESP_OK) {
+        return ESP_FAIL;
+    }
+    if (httpd_query_key_value(buf, "reg", _reg, sizeof(_reg)) != ESP_OK ||
+        httpd_query_key_value(buf, "mask", _mask, sizeof(_mask)) != ESP_OK ||
+        httpd_query_key_value(buf, "val", _val, sizeof(_val)) != ESP_OK) {
+        free(buf);
+        httpd_resp_send_404(req);
+        return ESP_FAIL;
+    }
+    free(buf);
+
+    int reg = atoi(_reg);
+    int mask = atoi(_mask);
+    int val = atoi(_val);
+    ESP_LOGI(TAG, "Set Register: reg: 0x%02x, mask: 0x%02x, value: 0x%02x", reg, mask, val);
+
+    sensor_t *s = esp_camera_sensor_get();
+    int res = s->set_reg(s, reg, mask, val);
     if (res) {
         return httpd_resp_send_500(req);
     }
