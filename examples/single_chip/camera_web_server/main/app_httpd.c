7a1c3471a6719f9bf42cfc69508d55803fbb9847
Schleichdie <Lucas.Lenarcic@gmail.com>
2022-08-02 09:16:34 +0200

    Esp-Who Copy: Testing

--- examples/single_chip/camera_web_server/main/app_httpd.c
+++ examples/single_chip/camera_web_server/main/app_httpd.c
@@ -261,8 +261,8 @@ static int run_face_recognition(dl_matrix3du_t *image_matrix, box_array_t *net_b
             {
                 ESP_LOGW(TAG, "Match Face ID: %d", matched_id);
                 rgb_printf(image_matrix, FACE_COLOR_GREEN, "Hello Subject %s", matched_id);
-                //ESP_LOGW(TAG, "Matched Face ID: %d", matched_id);
-                //ESP_LOGW(TAG, "Last ID: %d",last_id);
+                ESP_LOGW(TAG, "Matched Face ID: %d", matched_id);
+                ESP_LOGW(TAG, "Last ID: %d",last_id);
             }
             else
             {
@@ -1127,32 +1127,7 @@ static esp_err_t monitor_handler(httpd_req_t *req)
 static esp_err_t name_handler(httpd_req_t *req)
 {
     ESP_LOGE(TAG, "Fuuuuuuuucccccckkkk");
-    char *buf = NULL;
-    char variable[32];
-    char value[32];
-
-    if (parse_get(req, &buf) != ESP_OK) {
-        return ESP_FAIL;
-    }
-    if (httpd_query_key_value(buf, "var", variable, sizeof(variable)) != ESP_OK){
-        free(buf);
-        httpd_resp_send_404(req);
-        return ESP_FAIL;
-    }
-
-
-
-    strcpy(name_set[last_id], variable);
-    free(buf);
     
-    int k;
-    for (k = 0; k < 20; ++k)
-    {
-       ESP_LOGE(TAG, "Name: %s", name_set[k]);
-    }
-
-    
-
     httpd_resp_set_hdr(req, "Access-Control-Allow-Origin", "*");
     return httpd_resp_send(req, NULL, 0);
 }
