50932a9b577e81e7cd08afee7b466d4b39ab6f4d
Schleichdie <Lucas.Lenarcic@gmail.com>
2022-08-02 09:32:46 +0200

    Esp-Who Copy: Testing

--- examples/single_chip/camera_web_server/main/app_httpd.c
+++ examples/single_chip/camera_web_server/main/app_httpd.c
@@ -260,7 +260,7 @@ static int run_face_recognition(dl_matrix3du_t *image_matrix, box_array_t *net_b
             if (matched_id >= 0)
             {
                 ESP_LOGW(TAG, "Match Face ID: %d", matched_id);
-                rgb_printf(image_matrix, FACE_COLOR_GREEN, "Hello Subject %d", matched_id);
+                rgb_printf(image_matrix, FACE_COLOR_GREEN, "Hello Subject %s", name_set[matched_id]);
                 ESP_LOGW(TAG, "Matched Face ID: %d", matched_id);
                 ESP_LOGW(TAG, "Last ID: %d",last_id);
             }
